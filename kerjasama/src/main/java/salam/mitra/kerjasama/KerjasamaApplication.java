8194d806bd61ac3287b5b4a7542984492a164258
Alia Widyanita P <alia.widyanita@ui.ac.id>
2024-02-18 09:45:48 +0700

    feat: initialize environment

---
+++ kerjasama/src/main/java/salam/mitra/kerjasama/KerjasamaApplication.java
@@ -0,0 +1,13 @@
+package salam.mitra.kerjasama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerjasamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KerjasamaApplication.class, args);
+	}
+
+}
---
+++ kerjasama/src/test/java/salam/mitra/kerjasama/KerjasamaApplicationTests.java
@@ -0,0 +1,13 @@
+package salam.mitra.kerjasama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerjasamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
