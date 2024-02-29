082708090994a95abd991e63ef0fad1dd43ed40c
Alia Widyanita P <alia.widyanita@ui.ac.id>
2024-02-29 21:42:53 +0700

    refactor: monolithic service

---
+++ salamMitra/src/main/java/propensist/salamMitra/SalamMitraApplication.java
@@ -0,0 +1,13 @@
+package propensist.salamMitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalamMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalamMitraApplication.class, args);
+	}
+
+}
---
+++ salamMitra/src/main/java/propensist/salamMitra/controller/BaseController.java
@@ -0,0 +1,16 @@
+package propensist.salamMitra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        
+        return "landing-page";
+    }
+}
