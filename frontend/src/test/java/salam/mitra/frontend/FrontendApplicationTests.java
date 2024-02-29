13666f7012ce47ed34ea027f2e6343404d965b9d
Alia Widyanita P. <alia.widyanita@ui.ac.id>
2024-02-29 14:12:46 +0700

    Development

---
+++ frontend/src/test/java/salam/mitra/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package salam.mitra.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
