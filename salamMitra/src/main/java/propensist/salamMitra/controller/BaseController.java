744f78bcefe4bf864335b103c315f571c4fb1061
Alia Widyanita P <alia.widyanita@ui.ac.id>
2024-03-06 20:07:22 +0700

    feat: getting user logged in

--- salamMitra/src/main/java/propensist/salamMitra/controller/BaseController.java
+++ salamMitra/src/main/java/propensist/salamMitra/controller/BaseController.java
@@ -1,16 +1,60 @@
 package propensist.salamMitra.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.slf4j.LoggerFactory;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import propensist.salamMitra.dto.request.PenggunaDTO;
+import propensist.salamMitra.service.FrontEndService;
+import propensist.salamMitra.service.JwtService;
+import propensist.salamMitra.service.PenggunaService;
+
+import java.util.UUID;
+
+import org.slf4j.Logger;
 import org.springframework.ui.Model;
 
 @Controller
 public class BaseController {
 
+    Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    FrontEndService frontEndService;
+
+    @Autowired
+    PenggunaService penggunaService;
+
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, @CookieValue(value = "jwtToken", defaultValue = "") String jwtToken, HttpServletRequest request, RedirectAttributes redirectAttrs) {
+        PenggunaDTO userDTO = null;
+
+        if (!frontEndService.validateCookieJwt(request, jwtToken)) {
+            logger.info("Not logged in");
+        } else {
+            UUID id = jwtService.getIdFromJwtToken(jwtToken);
+
+            try {
+                userDTO = penggunaService.getUser(id, jwtToken);
+            } catch (RuntimeException e) {
+                redirectAttrs.addFlashAttribute("error", "Your session has expired. Please log in again");
+                return "redirect:/logout";
+            }
+
+            logger.info("Pengguna logged in: " + jwtToken);
+        }
         
+
+        model.addAttribute("user", userDTO);
+
         return "landing-page";
     }
 }
---
+++ salamMitra/src/main/java/propensist/salamMitra/dto/request/PenggunaDTO.java
@@ -0,0 +1,10 @@
+package propensist.salamMitra.dto.request;
+
+import lombok.Data;
+
+@Data
+public class PenggunaDTO {
+    private String username;
+    private String email;
+    private String password;
+}
