1fc8b7d7e595a6ca2003d6356d973a0612da896d
Made Indri Maharani Natiadewi <made.indri11@ui.ac.id>
2024-04-13 21:53:40 +0700

    Indri

--- salamMitra/src/main/java/propensist/salamMitra/service/PengajuanServiceImpl.java
+++ salamMitra/src/main/java/propensist/salamMitra/service/PengajuanServiceImpl.java
@@ -7,7 +7,9 @@ import propensist.salamMitra.model.Pengajuan;
 import propensist.salamMitra.repository.PengajuanDb;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Base64;
+import java.util.HashMap;
 
 
 
@@ -57,6 +59,25 @@ public class PengajuanServiceImpl implements PengajuanService{
         byte[] dokumenByte = pengajuan.getDokumen();
         String dokumen = Base64.getEncoder().encodeToString(dokumenByte);
         pengajuan.setDokumenBase64(dokumen); 
+    }
+
+    @Override
+    public void handleLaporan(Pengajuan pengajuan) {
+        byte[] laporanByte = pengajuan.getLaporan();
+        String laporan = Base64.getEncoder().encodeToString(laporanByte);
+        pengajuan.setLaporanBase64(laporan); // Set the imageBase64 field
+    }
+
+    @Override
+    public Map<String, Long> jumlahPengajuanByStatus() {
+        List<Object[]> counts = pengajuanDb.jumlahPengajuanByStatus();
+        Map<String, Long> countMap = new HashMap<>();
+        for (Object[] obj : counts) {
+            String status = (String) obj[0];
+            Long count = (Long) obj[1];
+            countMap.put(status, count);
+        }
+        return countMap;
     }
 
 }
