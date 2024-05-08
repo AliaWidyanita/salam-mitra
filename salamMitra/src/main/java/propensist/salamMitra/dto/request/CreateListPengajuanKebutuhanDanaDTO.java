8e3b496568b3b74ab35865533556d9825e8db50c
Alia Widyanita P <alia.widyanita@ui.ac.id>
2024-05-08 10:53:54 +0700

    fix: html error and unnecessary imports

--- salamMitra/src/main/java/propensist/salamMitra/dto/request/CreateListPengajuanKebutuhanDanaDTO.java
+++ salamMitra/src/main/java/propensist/salamMitra/dto/request/CreateListPengajuanKebutuhanDanaDTO.java
@@ -1,26 +1,7 @@
 package propensist.salamMitra.dto.request;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
-
-import java.math.BigInteger;
-import lombok.Data;
 
 import java.util.List;
 
